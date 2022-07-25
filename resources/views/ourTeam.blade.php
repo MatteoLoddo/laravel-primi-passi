63ffb789066eaf83c87b098db958e6a38b632053
MatteoLoddo <loddomatteo96@gmail.com>
2022-07-25 15:06:49 +0200

    print some content with Cycle in array and Condition

--- resources/views/ourTeam.blade.php
+++ resources/views/ourTeam.blade.php
@@ -1,3 +1,18 @@
+@php
+  $team = [
+    [
+      "name" => "Franco",
+      "surname" => "Lino",
+      "work" => "web-developer",
+],
+    [
+      "name" => "Piero",
+      "surname" => "Afgano",
+      "work" => "CEO",
+],
+];
+@endphp
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +23,19 @@
 </head>
 <body>
   <h1>Vi presento il nostro Team</h1>
+  <ul>
+    @foreach ($team as $member)
+        <li>{{$member["name"]}} {{$member["surname"]}}</li>
+      
+            @if ($member["work"] == "CEO")
+              <h5>IO SONO IL PADRONE</h5>
+            @else
+              <h5>IO SONO UN DIPENDENTE</h5>
+            @endif
+        
+
+    @endforeach
+  </ul>
   
 </body>
 </html>
