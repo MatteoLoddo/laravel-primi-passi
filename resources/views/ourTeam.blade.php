5ab19285319029e29e5166ea22532764d1cf5eaa
MatteoLoddo <loddomatteo96@gmail.com>
2022-07-25 19:05:51 +0200

    add courses page

--- resources/views/ourTeam.blade.php
+++ resources/views/ourTeam.blade.php
@@ -1,63 +1,66 @@
 @php
-  $team = [
+$team = [
     [
-      "name" => "Franco",
-      "surname" => "Lino",
-      "work" => "front-end Developer",
-      "img" => "https://picsum.photos/200?random=1",
-],
+        'name' => 'Franco',
+        'surname' => 'Lino',
+        'work' => 'front-end Developer',
+        'img' => 'https://picsum.photos/200?random=1',
+    ],
     [
-      "name" => "Piero",
-      "surname" => "Afgano",
-      "work" => "CEO",
-      "img" => "https://picsum.photos/200?random=2",
-],
-[
-      "name" => "Elisa",
-      "surname" => "Ferro",
-      "work" => "Designer",
-      "img" => "https://picsum.photos/200?random=3",
-],
-[
-      "name" => "Franco",
-      "surname" => "Lino",
-      "work" => "back-end Developer",
-      "img" => "https://picsum.photos/200?random=4",
-],
+        'name' => 'Piero',
+        'surname' => 'Afgano',
+        'work' => 'CEO',
+        'img' => 'https://picsum.photos/200?random=2',
+    ],
+    [
+        'name' => 'Elisa',
+        'surname' => 'Ferro',
+        'work' => 'Designer',
+        'img' => 'https://picsum.photos/200?random=3',
+    ],
+    [
+        'name' => 'Franco',
+        'surname' => 'Lino',
+        'work' => 'back-end Developer',
+        'img' => 'https://picsum.photos/200?random=4',
+    ],
 ];
 @endphp
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <title>Document</title>
 </head>
+
 <body>
-  <h1 class="text-center p-3">Vi presento il nostro Team</h1>
-  <div class="container">
-  <div class="row m-auto">
-    @foreach ($team as $member)
-    <div class="col">
-      <ul class="list-unstyled">
-        <img src="{{$member["img"]}}" alt="">
-          <li>{{$member["name"]}} {{$member["surname"]}}</li>
-          <li>{{$member["work"]}}</li>
-              @if ($member["work"] == "CEO")
-                <h5>CAPO</h5>
-              @else
-                <h5>DIPENDENTE</h5>
-              @endif
-          
-      </ul>
+    <h1 class="text-center p-3">Vi presento il nostro Team</h1>
+    <div class="container">
+        <div class="row m-auto">
+            @foreach ($team as $member)
+                <div class="col">
+                    <ul class="list-unstyled">
+                        <img src="{{ $member['img'] }}" alt="">
+                        <li>{{ $member['name'] }} {{ $member['surname'] }}</li>
+                        <li>{{ $member['work'] }}</li>
+                        @if ($member['work'] == 'CEO')
+                            <h5>CAPO</h5>
+                        @else
+                            <h5>DIPENDENTE</h5>
+                        @endif
+
+                    </ul>
+                </div>
+            @endforeach
+        </div>
     </div>
-  
-    @endforeach
-  </div>
-</div>
-  
+
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,46 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+// homepage
 Route::get('/', function(){
-    return view("Hello") ;
+    return view ("Hello") ;
 });
 
 
-
+// pagina our-team
 Route::get('/our-team', function(){
     
     return view("ourTeam");
-})->name("OurTeam");
+})->name("ourTeam");
 
 
+// pagina che ritorna un array convertito in json
+Route::get('/corsi' , function(){
+    $corsi = [
+        [
+            "title" => "Front-end",
+            "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident debitis porro incidunt!",
+        ],
+        [
+            "title" => "Back-end",
+            "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident debitis porro incidunt!Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam exercitationem veritatis nisi repellendus modi quisquam incidunt possimus dolorem ullam, error odio praesentium magni!",
+        ],
+        [
+            "title" => "FullStack",
+            "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident debitis porro incidunt! Lorem ipsum dolor sit amet.",
+        ],
+        [
+            "title" => "Design",
+            "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident debitis porro incidunt! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, neque vero. Sapiente, ab nostrum ea eaque natus aut nemo, aperiam ut voluptatem suscipit ducimus consequuntur at ullam. Accusantium fugit ipsum alias, iste minima ipsa?",
+        ],
+        [
+            "title" => "Database",
+            "description" => "Lorem ipsum dolor sit amet consectetur "
+        ],
+    ];
 
+return view("courses" , [
+    "coursesList" => $corsi
+]);
+})->name("courses");
